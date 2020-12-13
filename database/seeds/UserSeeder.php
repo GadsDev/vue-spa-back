3eca7221eea069a3ee51165d48f87a81a00afe05
gustavo.silva <gustavo.silva@codificar.com.br>
2020-12-13 17:40:44 -0300

    User Seed

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(\App\User::class)->create([
+            'email' => 'gusttavo212@gmail.com',
+            'password' => 'senha123'
+        ]);
+    }
+}
