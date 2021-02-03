5872cb6d1fc9f53223d590c29f6bd82554c6dc3f
gustavo.silva <gustavo.silva@codificar.com.br>
2021-02-03 20:55:11 -0300

    Alter Laravel Default Exception format

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,9 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
 {
@@ -50,6 +51,17 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+        if($exception instanceof ModelNotFoundException) {
+            $modelName = class_basename($exception->getModel());
+            $apiErrorCode = $modelName. 'NotFoundException';
+            $message = $modelName. ' not found.';
+
+            return response()->json([
+                'error' => $apiErrorCode,
+                'message' => $message,
+            ], 404);
+        }
+
         return parent::render($request, $exception);
     }
 }
