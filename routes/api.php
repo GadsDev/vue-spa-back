abff22e057b98d7720e6ffced798474be44ebfe3
gustavo.silva <gustavo.silva@codificar.com.br>
2021-02-07 09:10:39 -0300

    Store Todo

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,7 @@ Route::prefix('v1')->group(function () {
     Route::prefix('todos')->group(function () {
 
         Route::get('/', [TodoController::class, 'index']);
+        Route::post('/', [TodoController::class, 'store']);
 
     });
 
